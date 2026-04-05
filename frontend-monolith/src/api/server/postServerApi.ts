7a0ae83bb49117dcc47a5413f212ccff658c03d3
oleksandr <oleksandr.murin@nure.ua>
2026-04-05 23:21:53 +0200

    feat: add search, sort and order to posts feed in monolith

--- frontend-monolith/src/api/server/postServerApi.ts
+++ frontend-monolith/src/api/server/postServerApi.ts
@@ -1,12 +1,12 @@
+import { GetPostsParams } from "@/app/(main)/page";
 import { Post } from "@/types/apiTypes";
+import { postQueryBuilder } from "@/utils/postQueryBuilder";
 import { cookies } from "next/headers";
 
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
-export async function getAllPosts(category?: string): Promise<Post[]> {
-  const params = new URLSearchParams();
-  if (category) params.set("category", category);
-  const query = params.toString();
+export async function getAllPosts(params: GetPostsParams): Promise<Post[]> {
+  const query = postQueryBuilder(params);
   const url = query
     ? `${API_BASE_URL}/posts?${query}`
     : `${API_BASE_URL}/posts`;
--- frontend-monolith/src/app/(main)/page.tsx
+++ frontend-monolith/src/app/(main)/page.tsx
@@ -4,26 +4,43 @@ import FeedControlsBar from "@/components/FeedControlsBar";
 import { PostCard } from "@/components/PostCard";
 import { Container, Stack } from "@mui/material";
 
+export type GetPostsParams = {
+  search?: string;
+  sortBy?: "createdAt";
+  order?: "ASC" | "DESC";
+  category?: string;
+};
+
 type Props = {
-  searchParams: Promise<{ category?: string }>;
+  searchParams: Promise<GetPostsParams>;
 };
 
 const FeedPage = async ({ searchParams }: Props) => {
   const params = await searchParams;
-  const category = params.category;
-  const posts = await getAllPosts(category);
+  const posts = await getAllPosts(params);
   const categories = await getAllCategories();
 
   return (
-    <Container maxWidth="sm" sx={{ py: 4, mt: 5 }}>
-      <FeedControlsBar
-        categories={categories}
-        selectedCategory={category ?? "all"}
-      />
-      <Stack spacing={3}>
-        {posts.map((post) => {
-          return <PostCard key={post.id} post={post} />;
-        })}
+    <Container
+      sx={{
+        py: 4,
+        mt: 8,
+      }}
+    >
+      <Stack spacing={3} alignItems="center">
+        <FeedControlsBar categories={categories} />
+
+        <Stack
+          spacing={3}
+          sx={{
+            width: "100%",
+            maxWidth: 500,
+          }}
+        >
+          {posts.map((post) => {
+            return <PostCard key={post.id} post={post} />;
+          })}
+        </Stack>
       </Stack>
     </Container>
   );
