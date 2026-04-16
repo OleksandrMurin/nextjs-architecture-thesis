7198ba687068e512b5140d22c338c6a33cbb62b0
oleksandr <oleksandr.murin@nure.ua>
2026-04-16 03:15:19 +0200

    ui: adjust layout of posts feed on my-posts page

--- frontend-fsd/src/app/(main)/my-posts/page.tsx
+++ frontend-fsd/src/app/(main)/my-posts/page.tsx
@@ -1,10 +1,23 @@
 import { getMyPosts } from "@/entities/post";
 import { PostFeed } from "@/widgets/post-feed";
+import { Stack } from "@mui/material";
 
 const UserPostsPage = async () => {
   const posts = await getMyPosts();
 
-  return <PostFeed posts={posts} />;
+  return (
+    <Stack spacing={3} alignItems="center">
+      <Stack
+        spacing={3}
+        sx={{
+          width: "100%",
+          maxWidth: 500,
+        }}
+      >
+        <PostFeed posts={posts} />
+      </Stack>
+    </Stack>
+  );
 };
 
 export default UserPostsPage;
