5b53fd1446f3c5fc6e576b9633f9c20f5e6ff515
oleksandr <oleksandr.murin@nure.ua>
2026-05-15 21:38:41 +0200

    fix: use shared post form for post creation in fsd

--- frontend-fsd/src/app/(main)/new-post/page.tsx
+++ frontend-fsd/src/app/(main)/new-post/page.tsx
@@ -1,7 +1,30 @@
 "use client";
 
-import { CreatePostForm } from "@/features/create-post";
+import { Category } from "@/entities/post";
+import PostForm, { PostFormValues } from "@/entities/post/ui/PostForm";
+import { createPost } from "@/features/create-post/api/create-post";
+import { getAllCategories } from "@/features/create-post/api/get-categories";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function CreatePostPage() {
-  return <CreatePostForm />;
+  const router = useRouter();
+  const [categories, setCategories] = useState<Category[]>([]);
+
+  useEffect(() => {
+    async function loadCategories() {
+      const data = await getAllCategories();
+      setCategories(data);
+    }
+
+    loadCategories();
+  }, []);
+
+  async function onSubmit(values: PostFormValues) {
+    await createPost(values.image!, values.description, values.categoryId);
+
+    router.push("/my-posts");
+  }
+
+  return <PostForm mode="create" categories={categories} onSubmit={onSubmit} />;
 }
